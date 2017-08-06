9d96da5b272a3604584bd5b09b5e802ec702c389
qiutuleng <qiutuleng@gmail.com>
2017-08-07 01:04:02 +0800

    Add article list spider

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const axios = require('axios');
 
+let domain = 'https://laravel-news.com';
+let listPage = '/category/laravel-5.5';
+
+axios.get(domain + listPage)
+    .then(console.log)
+    .catch(console.log);
