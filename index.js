26489f5dbfc252205e2e3eb8a74cc459708b738d
qiutuleng <qiutuleng@gmail.com>
2017-08-07 01:35:25 +0800

    Improve article spider

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 const axios = require('axios');
+const cheerio = require('cheerio');
+const watch = require('watchjs').watch;
 
 let domain = 'https://laravel-news.com';
-let listPage = '/category/laravel-5.5';
+let articleListPage = '/category/laravel-5.5';
 
-axios.get(domain + listPage)
-    .then(console.log)
-    .catch(console.log);
+let articleListSelector = 'a.card';
+let articleTitleSelector = 'h1.post__title';
+let articleContentSelector = 'div.col.lg-col-9';
+
+let data = {
+    articles: {
+        urls: [],
+        list: []
+    }
+};
+
+watch(data.articles, 'urls', (prop, action, value, old_value) => {
+    getArticle(value[0]);
+});
+
+axios.get(domain + articleListPage)
+    .then(response => {
+        let $ = cheerio.load(response.data);
+        let list = $(articleListSelector);
+        for (let i = 0; i < list.length; i++) {
+            data.articles.urls.push(domain + list.eq(i).attr('href'));
+        }
+    });
+
+let getArticle = url => {
+    axios.get(url)
+        .then(response => {
+            let $ = cheerio.load(response.data);
+            let article = {
+                title: $(articleTitleSelector).text(),
+                content: $(articleContentSelector).html()
+            };
+            data.articles.list.push(article);
+        });
+};
