fd0376464676b5573c864077cb978187ac83db96
qiutuleng <qiutuleng@gmail.com>
2017-08-29 00:09:47 +0800

    Improve mysql config

--- MySQL/MySQL.js
+++ MySQL/MySQL.js
@@ -1,10 +1,13 @@
 const MySQL = require('mysql');
 
+let mysqlConfig  = config('database.connections.mysql');
 let connection = MySQL.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'spider'
+    host: mysqlConfig.host,
+    port: mysqlConfig.port,
+    user: mysqlConfig.username,
+    password: mysqlConfig.password,
+    database: mysqlConfig.database,
+    charset: mysqlConfig.charset,
 });
 
 connection.connect(error => {
